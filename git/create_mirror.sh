83508ed8e8677e6cb9059fff952b8ef166e00fa3
cephgit <cephgit@ds3426.dreamservers.com>
2015-04-16 08:28:11 -0700

    enable git-archive on the main repowq y

--- git/create_mirror.sh
+++ git/create_mirror.sh
@@ -7,6 +7,7 @@ BINDIR=$HOME/git
 cd $GITDIR
 test -d $1.git && rm -rf $1.git
 git clone --bare --mirror git://github.com/ceph/$1
+cat $BINDIR/config.fragment.txt >> $GITDIR/$1.git/config
 
 cd $MIRRORDIR
 test -d $1.git && rm -rf $1.git
@@ -14,7 +15,7 @@ git clone --bare git://github.com/ceph/$1
 cd $1.git
 echo > description
 ln -s $BINDIR/post-receive-email hooks/post-receive
-cat $BINDIR/config.fragment.txt >> config
+cat $BINDIR/mirror.fragment.txt >> config
 
 git remote set-url origin $GITDIR/$1.git
 
