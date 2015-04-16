b27b0a43aca9690f4dff8db56d2832cda85d4f9d
cephgit <cephgit@ds3426.dreamservers.com>
2015-04-16 15:33:33 -0700

    add linux-firmware.git. add ability to use non-standard origin

--- git/create_mirror.sh
+++ git/create_mirror.sh
@@ -4,14 +4,17 @@ GITDIR=$HOME/git.ceph.com
 MIRRORDIR=$HOME/git/mirror
 BINDIR=$HOME/git
 
+origin=$2
+[ -z "$origin" ] && origin=git://github.com/ceph/$1
+
 cd $GITDIR
 test -d $1.git && rm -rf $1.git
-git clone --bare --mirror git://github.com/ceph/$1
+git clone --bare --mirror $origin
 cat $BINDIR/config.fragment.txt >> $GITDIR/$1.git/config
 
 cd $MIRRORDIR
 test -d $1.git && rm -rf $1.git
-git clone --bare git://github.com/ceph/$1
+git clone --bare $origin
 cd $1.git
 echo > description
 ln -s $BINDIR/post-receive-email hooks/post-receive
